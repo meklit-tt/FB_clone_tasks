f798c5d1be0c93d5b891e932a84d093ea15c28c4
meklit <meklit_teshome@diveintocode.jp>
2020-11-25 17:51:22 +0900

    controller edited

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -59,4 +59,12 @@ class PostsController < ApplicationController
       format.json { head :no_content }
     end
   end
+  private
+ def set_post
+   @post = Post.find(params[:id])
+ end
+
+ def post_params
+   params.require(:post).permit(:image, :image_cache, :content)
+ end 
 end
